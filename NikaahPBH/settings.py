940e3c083f454909ea6f0fc6a83a4cb35b475963
Armankhan2001 <129832036+Armankhan2001@users.noreply.github.com>
2024-04-06 17:25:09 +0530

    added hostname

--- NikaahPBH/settings.py
+++ NikaahPBH/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!az*i%+tw#ez+!27$gadi(0m8euqs(jf3n2&@k8)y-fo)dgqb)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','https://nikaahpbh-59ead36c8eb6.herokuapp.com/']
 
 
 # Application definition
