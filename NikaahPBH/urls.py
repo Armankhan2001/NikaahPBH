a6a946e9cb7d3f9eb2d8847932779308ca6e7964
Armankhan2001 <129832036+Armankhan2001@users.noreply.github.com>
2024-04-08 00:58:15 +0530

    Update urls.py

--- NikaahPBH/urls.py
+++ NikaahPBH/urls.py
@@ -14,41 +14,67 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.contrib import admin
+# from django.urls import path
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+# from django.urls import path
+# from PBH.views import create_profile, profile_created,get_profiles,update_profile,get_profile,index,contact_us
+
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from django.views.static import serve
+# from django.conf.urls import url
+
+# # from django.views.generic import TemplateView
+# print("hello urls")
+# print("hello")
+# urlpatterns = [
+#     path('', index, name='index'),
+#     path('admin/', admin.site.urls),
+#     path('create-profile/', create_profile, name='create_profile'),
+#     path('profile-created/', profile_created, name='profile_created'),
+#     path('update-profile/',update_profile, name='update_profile'),
+#     path('profiles/', get_profiles, name='get_profiles'),
+#     path('profiles/<int:profile_id>/', get_profile, name='get_profile'),
+#     path('contact_us/', contact_us, name='contact us'),
+#     url(r'^media/(?p<path>.*)$',serve,{'document_root': settings.MEDIA_ROOT}),
+#     url(r'^media/(?p<path>.*)$',serve,{'document_root': settings.STATIC_ROOT}),
+#     # path('', TemplateView.as_view(template_name='NikaahPBH/index.html'), name='index'),
+# ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# # ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+
+
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
 from django.contrib import admin
-from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
-
-from django.urls import path
-from PBH.views import create_profile, profile_created,get_profiles,update_profile,get_profile,index,contact_us
-
+from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-from django.conf.urls import url
 
-# from django.views.generic import TemplateView
-print("hello urls")
-print("hello")
+from PBH.views import create_profile, profile_created, get_profiles, update_profile, get_profile, index, contact_us
+
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('create-profile/', create_profile, name='create_profile'),
     path('profile-created/', profile_created, name='profile_created'),
-    path('update-profile/',update_profile, name='update_profile'),
+    path('update-profile/', update_profile, name='update_profile'),
     path('profiles/', get_profiles, name='get_profiles'),
     path('profiles/<int:profile_id>/', get_profile, name='get_profile'),
-    path('contact_us/', contact_us, name='contact us'),
-    url(r'^media/(?p<path>.*)$',serve,{'document_root': settings.MEDIA_ROOT}),
-    url(r'^media/(?p<path>.*)$',serve,{'document_root': settings.STATIC_ROOT}),
-    # path('', TemplateView.as_view(template_name='NikaahPBH/index.html'), name='index'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-# ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
-
+    path('contact_us/', contact_us, name='contact_us'),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
